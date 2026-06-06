9db5773566ed72bb306325bef70f57c20cfd5029
edgardopaz <dedgardo381@gmail.com>
2026-06-06 16:10:33 -0400

    addEntry and newService done

--- internal/vault/service.go
+++ internal/vault/service.go
@@ -1,13 +1,16 @@
 package vault
 
+import "errors"
+
 type Store interface {
-	// TODO: Implememnt the store interface to store the encrypted entries
-	NewService(store Store, masterPassword string) *Service
-	AddEntry(entry Entry) error
-	GetEntry(id int) (Entry, error)
-	ListEntries() ([]Entry, error)
-	UpdateEntry(entry Entry) error
+	// TODO: Implememnt the store interface using EncryptedEntry + salt
+	AddEntry(entry EncryptedEntry) error
+	GetEntry(id int) (EncryptedEntry, error)
+	ListEntries() ([]EncryptedEntry, error)
+	UpdateEntry(entry EncryptedEntry) error
 	DeleteEntry(id int) error
+	GetSalt() ([]byte, error)
+	SaveSalt(salt []byte) error
 }
 
 type Service struct {
@@ -15,11 +18,66 @@ type Service struct {
 	key []byte
 }
 
-NewService(store Store, masterPassword string) *Service {
-	salt, err := generateSalt()
-	if err != nil {
+var ErrNoSalt = errors.New("no salt found")
+
+func NewService(store Store, masterPassword string) (*Service, error) {
+	salt, err := store.GetSalt()
+
+	switch {
+	case errors.Is(err,ErrNoSalt):
+
+		salt, err = generateSalt()
+		if err != nil {
+			return nil, err
+		}
+
+		if err := store.SaveSalt(salt); err != nil {
+			return nil, err
+		}
+	case err != nil:
 		return nil, err
 	}
+
 	key := deriveKey(masterPassword, salt)
 	return &Service{store: store, key: key}, nil
+}
+
+func (s *Service) AddEntry(entry Entry) error {
+	// encrypt the entry using the key
+	encryptedUsername, err := encrypt([]byte(entry.Username), s.key)
+	if err != nil {
+		return err
+	}
+	encryptedURL, err := encrypt([]byte(entry.URL), s.key)
+	if err != nil {
+		return err
+	}
+	encryptedNotes, err := encrypt([]byte(entry.Notes), s.key)
+	if err != nil {
+		return err
+	}
+	encryptedPassword, err := encrypt([]byte(entry.Password), s.key)
+	if err != nil {
+		return err
+	}
+	encryptedEntry := EncryptedEntry{
+		ID: entry.ID,
+		Username: encryptedUsername,
+		Password: encryptedPassword,
+		URL: encryptedURL,
+		Notes: encryptedNotes,
+	}
+	return s.store.AddEntry(encryptedEntry)
+}
+func (s *Service) GetEntry(id int) (Entry, error) {
+	return Entry{}, nil	
+}
+func (s *Service) ListEntries() ([]Entry, error) {
+	return []Entry{}, nil
+}
+func (s *Service) UpdateEntry(entry Entry) error {
+	return nil
+}
+func (s *Service) DeleteEntry(id int) error {
+	return nil
 }
